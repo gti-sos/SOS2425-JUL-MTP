8f994391ba08e14e5075687c8bbd73471eba53d6
mariotuur <marturpan@alum.us.es>
2025-06-14 20:02:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 16078;
+
+app.get('/hello', (request, response)=> {
+    response.send('Hello World!');
+});
+
+app.use("/", express.static('./public')); //static files
+
+app.listen(PORT, () =>{
+    console.log(`Server is listening on port ${PORT}`);
+}); //listen es asincrono de modo que tarda un pelin
